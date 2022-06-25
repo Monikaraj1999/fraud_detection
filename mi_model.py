5532ca8e5dea7193501a474c6ad19a4df762d728
Monikaraj1999 <monikaraj1999@gmail.com>
2022-06-25 22:29:21 +0530

    added 3 new ml model

--- mi_model.py
+++ mi_model.py
@@ -1,3 +1,15 @@
 
 
 print("Adding all ML related code here")
+
+
+def model1():
+    print("adding core for ML Model1")
+
+
+def model2():
+    print("adding core for ML Model2")
+
+
+def model3():
+    print("adding core for ML Model3")
